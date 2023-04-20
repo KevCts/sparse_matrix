bea0cea28bb8254615e6dc0af1fb2f6f0133b42d
Kevin Coetsier <kevin.coetsier@outlook.fr>
2023-04-20 21:40:06 +0200

    Adding conjugate gradient solver

--- src/coo_mat.rs
+++ src/coo_mat.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, vec};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct CooMat {
     rows:usize,
     columns:usize,
@@ -38,6 +38,89 @@ impl CooMat {
             }
         }
         dense
+    }
+
+    pub fn transpose (&self) -> CooMat {
+        let mut result = CooMat::new(self.columns, self.rows);
+
+        for ((i,j), value) in &self.coordinates {
+            result.add_value(*j, *i, *value).expect("An error occured while transposing");
+        }
+
+        result
+    }
+
+    pub fn to_scalar(&self) -> Result<f32, &str> {
+        if self.rows == 1 && self.columns == 1 {
+            Ok(self[(0,0)])
+        }
+
+        else {
+            Err("This matrix isn't a scalar")
+        }
+    }
+
+    pub fn conjugate_gradient_solve(&self, b:CooMat) -> Result<CooMat, &'static str> {
+        if b.rows != self.rows {
+            Err("Invalid size for b vector")
+        }
+        else {
+            let mut x = CooMat::new(self.rows, 1);
+
+            for i in 0..self.rows{
+                x.add_value(i, 1, 0.).unwrap();
+            }
+
+            let mut r = (b.clone() - (self.clone()*x.clone()).unwrap()).unwrap();
+
+            let mut rm1 : CooMat;
+
+            let mut p = r.clone();
+
+            let mut a : f32;
+
+            let mut b : f32;
+
+            for _ in 0..2*self.rows {
+                a = ((r.clone().transpose() * r.clone()).unwrap().to_scalar().unwrap()) / ((p.clone().transpose() * (self.clone() * p.clone()).unwrap()).unwrap().to_scalar().unwrap());
+                x = (x + p.clone() * a).unwrap();
+                rm1 = r.clone();
+                r = (r - (self.clone() * p.clone()).unwrap() * a).unwrap();
+                b = (r.clone().transpose() * r.clone()).unwrap().to_scalar().unwrap() / ((rm1.clone().transpose() * rm1.clone()).unwrap().to_scalar().unwrap());
+                p = (r.clone() + (p * b)).unwrap();
+            }
+
+            Ok(x)
+        }
+    }
+}
+
+impl std::ops::Sub<CooMat> for CooMat {
+    type Output = Result<CooMat, &'static str>;
+    fn sub(self, rhs: CooMat) -> Self::Output {
+        if self.rows != rhs.rows || self.columns != rhs.columns {
+            Err("Matrix dimensions aren't the same")
+        }
+        else {
+
+            let mut rescoo = HashMap::new();
+            let coo1 = self.coordinates;
+            let coo2 = rhs.coordinates;
+            for ((i, j), value) in coo1 {
+                rescoo.insert((i, j), value);
+            }
+            for ((i, j), value) in coo2 {
+                rescoo
+                    .entry((i, j))
+                    .and_modify(|a| *a -= value)
+                    .or_insert(-1.*value);
+            }
+            Ok(CooMat {
+                rows: self.rows,
+                columns: self.columns,
+                coordinates: rescoo,
+            })
+        }
     }
 }
 
@@ -150,5 +233,17 @@ impl std::ops::Mul<Vec<f32>> for CooMat {
 
             Ok(res)
         }
+    }
+}
+
+impl std::ops::Mul<f32> for CooMat {
+    type Output = CooMat;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        let mut result = CooMat::new(self.rows, self.columns);
+        for ((i, j), value) in self.coordinates {
+            result.add_value(i, j, rhs * value).unwrap();
+        }
+        result
     }
 }

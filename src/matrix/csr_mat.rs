0caaa8dec31da6716e9ed6b6d040eea106896722
Kevin Coetsier <kevin.coetsier@outlook.fr>
2023-07-29 18:12:36 +0000

    Solved a problem in matrix - vector product

--- src/matrix/csr_mat.rs
+++ src/matrix/csr_mat.rs
@@ -39,13 +39,13 @@ impl Mul<&Vector> for &CsrMat {
         if self.columns != rhs.values.len() {
             Err("Invalid shapes")
         } else {
-            let mut result = vec![];
+            let mut result = vec![0.; self.columns];
             for i in 0..self.rows_index.len()-1{
                 let mut sum = 0.;
                 for j in self.rows_index[i]..self.rows_index[i+1] {
                     sum += self.values[j] * rhs.values[self.columns_index[j]];
                 }
-                result.push(sum)
+                result[i] = sum;
             }
             Ok(Vector { values : result})
         }
